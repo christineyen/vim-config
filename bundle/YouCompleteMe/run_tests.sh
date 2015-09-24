6c3ed7a934cddef72b8dd6389a826bab3c3aba23
Christine Yen <cyen@christineyen.com>
2015-09-24 15:52:25 -0400

    Changes to YouCompleteMe?

--- bundle/YouCompleteMe/run_tests.sh
+++ bundle/YouCompleteMe/run_tests.sh
@@ -4,21 +4,40 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-$SCRIPT_DIR/third_party/ycmd/build.sh
+function usage {
+  echo "Usage: $0 [--skip-build]"
+  exit 0
+}
 
-flake8 --select=F,C9 --max-complexity=10 $SCRIPT_DIR/python
+flake8 --select=F,C9 --max-complexity=10 "${SCRIPT_DIR}/python"
+skip_build=false
 
-for directory in $SCRIPT_DIR/third_party/*; do
+for flag in $@; do
+  case "$flag" in
+    --skip-build)
+      skip_build=true
+      ;;
+    *)
+      usage
+      ;;
+  esac
+done
+
+if ! $skip_build; then
+    "${SCRIPT_DIR}/third_party/ycmd/build.py"
+fi
+
+for directory in "${SCRIPT_DIR}"/third_party/*; do
   if [ -d "${directory}" ]; then
     export PYTHONPATH=${directory}:$PYTHONPATH
   fi
 done
 
 
-for directory in $SCRIPT_DIR/third_party/ycmd/third_party/*; do
+for directory in "${SCRIPT_DIR}"/third_party/ycmd/third_party/*; do
   if [ -d "${directory}" ]; then
     export PYTHONPATH=${directory}:$PYTHONPATH
   fi
 done
 
-nosetests -v $SCRIPT_DIR/python
+nosetests -v "${SCRIPT_DIR}/python"
